0250d6a797feffef72cc7347cfe1046c1cb9ab02
Matt Price <matt.price1988@gmail.com>
2015-08-22 19:59:04 -0700

    Added some code to view music files

--- app/src/main/java/com/pitchchanger/matt/pitchchanger/MainActivity.java
+++ app/src/main/java/com/pitchchanger/matt/pitchchanger/MainActivity.java
@@ -1,9 +1,16 @@
 package com.pitchchanger.matt.pitchchanger;
 
+import android.database.Cursor;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +19,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> songs = findMusic();
+
+        ListView allMusic = (ListView) findViewById(R.id.allMusic);
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                songs );
+
+        allMusic.setAdapter(arrayAdapter);
     }
 
     @Override
@@ -34,5 +55,39 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public List<String> findMusic() {
+        //Some audio may be explicitly marked as not being music
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+
+        String[] projection = {
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DISPLAY_NAME,
+                MediaStore.Audio.Media.DURATION
+        };
+
+        Cursor cursor;
+        cursor = this.managedQuery(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                null,
+                null);
+
+        List<String> songs = new ArrayList<String>();
+        while(cursor.moveToNext()) {
+            songs.add(cursor.getString(0) + "||"
+                    + cursor.getString(1) + "||"
+                    + cursor.getString(2) + "||"
+                    + cursor.getString(3) + "||"
+                    + cursor.getString(4) + "||"
+                    + cursor.getString(5));
+        }
+
+        return songs;
     }
 }
